6523dbaa5c62df5d0327f8120c4725dbae5f5034
Anthony Beaver <anthonybeavermusic@gmail.com>
2020-09-10 18:51:22 -0700

    chores clicked pushes points to user points variable

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 'use strict';
 
+// Most of the global variables here.
 var users = [];
 var choreArray = [];
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var choreList = document.getElementById('chores_index');
 var dayList = document.getElementById('day_index');
 var choresForm = document.getElementById('chores_form');
+var ul = document.getElementById('todo');
+var sectionTodo = document.getElementById('section_todo');
+
+var userPoints = 0; // this will be part of the constructor.
 
 //login function
 function login() {
@@ -91,21 +96,21 @@ var addedChores = new ChoreAndDay([]);
 function fillToDo(event) {
   event.preventDefault();
   var choreName = event.target.chores_index.value;
-  console.log(choreName);
+  //console.log(choreName);
   var daySelection = event.target.day_index.value;
-  console.log(daySelection);
+  //console.log(daySelection);
   addedChores.addItem(choreName, daySelection);
   //console.log(addedChores); this works
   postToDoList();
 }
 //-----------------------------------------------
 
-var ul = document.getElementById('todo');
+
 var liRef = 0;
 var item = addedChores.item;
-function postToDoList () {
+var idName;
+function postToDoList() {
   var li = document.createElement('li');
-  var idName;
   for (var i = 0; i < item.length; i++) {
     idName = item[i].chores;
   }
@@ -115,5 +120,27 @@ function postToDoList () {
   liRef++;
 }
 
+//----------------------------- We left off here 9.9.2020
+function handleToDoCompleted(event) {
+  // thing 1, take it out
+  // thing 2, get the chore (name) and the choreArray.chore, grab the points assoiacted with that. Send that to a points global variable.
+
+  for (var i = 0; i < addedChores.item.length; i++) {
+    if (event.target.id === addedChores.item[i].chores) {
+      //console.log(`item instance chore name is, ${addedChores.item[i].chores}`);
+      break;
+    }
+  }
+  for (var j = 0; j < choreArray.length; j++) {
+    if (addedChores.item[i].chores === choreArray[j].chore) {
+      console.log(`item instance chore name is, ${addedChores.item[i].chores} choreArray is at ${choreArray[j].chores}`);
+      userPoints += choreArray[j].points;
+      console.log(`user points currently at ${userPoints}`);
+      break;
+    }
+  }
+}
+
 //event listener for filling the to-do list
 choresForm.addEventListener('submit', fillToDo);
+sectionTodo.addEventListener('click', handleToDoCompleted);
