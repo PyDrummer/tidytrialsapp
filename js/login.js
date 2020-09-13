29562a9d2a9b0c761c95213fd8bf75cc5c3552de
Matthew Holder <holdermatthew5@gmail.com>
2020-09-13 02:35:19 -0700

    update userPoint bar

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ var usersNameArray = ['dasad'];
 var usersArray = [];
 var loginForm = document.getElementById('loginForm');
 var registerUserForm = document.getElementById('registerUserForm');
-
+var currentUser = [];
 //var myFormNewUser = document.getElementById('newUserForm');
 
 
@@ -18,7 +18,14 @@ function Users(name) {
 var retrieveUser = localStorage.getItem('users');
 if (retrieveUser) {
   usersArray = JSON.parse(retrieveUser);
+} else {
+  new Users('xyz');
 }
+
+// var retrieveUser = localStorage.getItem('users');
+// if (retrieveUser) {
+//   usersArray = JSON.parse(retrieveUser);
+// }
 
 var retrieveUserName = localStorage.getItem('usersName');
 if (retrieveUserName) {
@@ -33,8 +40,17 @@ function login(event) {
   for (var i = 0; i < usersNameArray.length; i++) {
     //console.log('log2', usersNameArray);
     if (usersNameArray.includes(loginUser)) {
-      //console.log('log3',usersNameArray.includes(loginUser));
+      console.log('log3',usersNameArray.includes(loginUser));
       alert(`Welcome Back ${loginUser}`);
+      for (var j = 0; j < usersArray.length; j++) {
+        console.log(loginUser);
+        if (loginUser === usersArray[j].name) {
+          currentUser.push(usersArray[j]);
+          var userStr = JSON.stringify(currentUser);
+          localStorage.setItem('userObject', userStr);
+          console.log(currentUser);
+        }
+      }
       window.location.replace('');
       break;
     } else {
@@ -44,7 +60,6 @@ function login(event) {
   }
   loginForm.reset();
 }
-
 
 function registerUser(event) {
   event.preventDefault();
@@ -66,7 +81,7 @@ function registerUser(event) {
 }
 
 
-new Users('xyz');
+// new Users('xyz');
 
 
 
